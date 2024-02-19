8a1736a4ea8d2f47486d0c7547b3b47c1c05e1e7
Leejiheun <jiheun3124@gmail.com>
2024-02-20 00:27:20 +0900

    first

--- Algori/Test/1.py
+++ Algori/Test/1.py
@@ -1,8 +1,28 @@
-# 알고리즘 스터디
-new_list = []
-for _ in range(10):
-    T = int(input())
-    a = T % 42
-    if a not in new_list:
-        new_list.append(a)
-print(len(new_list))
+# 0219 백준 문제
+_ = input()
+N = list(map(int, input().split()))
+T = int(input())
+for _ in range(T):
+    s = list(map(int, input().split()))
+    if s[0] == 1: 
+        for n in range(len(N)):
+            if (n+1) % s[1] == 0:
+                if N[n] == 0:
+                    N[n] = 1
+                else:
+                    N[n] = 0
+        print(N)
+    if s[0] == 2:
+        for k in range(1, 101):
+            while N[s[1]-1]-k != N[s[1]-1]+k:
+                if N[s[1]-1]-k == N[s[1]-1]+k:
+                    if N[n] == 0:
+                        N[n] = 1
+                    else:
+                        N[n] = 0
+            print(N)
+
+
+    # while N(s[1]-1) :
+    # # if N[s[1]-1] == 0 and N[s[1]-2] == N[s[1]] and N[s[1]-3] == N[s[1]+1]:
+    #     print(N[s[1] - 1] == 1 and N[s[1]-2] == N[s[1]] == 0 and N[s[1]-3] == N[s[1]+1] == 1)
