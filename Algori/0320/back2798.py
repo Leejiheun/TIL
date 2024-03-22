e8e4a6407d3e150c1033ea1c36828735b64e9c0e
kaxadlec <kaxadlec@gmail.com>
2024-03-22 16:54:41 +0900

    first

--- Algori/0320/back2798.py
+++ Algori/0320/back2798.py
@@ -12,11 +12,11 @@ for i in range(len(lst)):
 
 n_lst.sort()
 check = []
+
 for j in range(len(n_lst)):
-    if M > n_lst[j]:continue
+    if M >= n_lst[j]:
+        check.append(n_lst[j])
     if M < n_lst[j]:
-        print(n_lst[j-1])
         break
-    if M == n_lst[j]:
-        print(n_lst[j])
-        break
+
+print(check[-1])
--- Algori/0321/baek2869.py
+++ Algori/0321/baek2869.py
@@ -1,10 +1,4 @@
 A, B, V = map(int, input().split())
 cnt = 0
-h = A - B
 
-while h <= V:
-
-    h = h + (A-B)
-    cnt += 1
-
-print(cnt-1)
+A - B, V - A
